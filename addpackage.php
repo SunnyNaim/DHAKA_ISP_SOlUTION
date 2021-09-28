64c8b40ec71d2a8e1e7d851cb5e7a39aaf4f4afd
hamim-ahmed <79504212+hamim-ahmed@users.noreply.github.com>
2021-09-29 00:12:08 +0600

    Added by Hamim(add new package)

--- addpackage.php
+++ addpackage.php
@@ -14,43 +14,43 @@
     <body>
         <div class='topbar'>
             <div class='topleft'>
-              <a class=' href='index.html'>Home</a>
-              <a href='usersignup.php'>USER-SIGNUP</a>
-              <a class=' href='userlogin.php'>USER-LOGIN</a>
+              <a class='' href='index.html'>My Info</a>
+              <a href='usersignup.php'>Users & Complain</a>
+              <!-- <a class=' href='userlogin.php'>USER-LOGIN</a> -->
             </div>
             <div class='topright'>
-              <a href='ispsignup.php'>ISP-SIGNUP</a>
-              <a class=' href='isplogin.php'>ISP-LOGIN</a>
-              <a href='mypanel.php'>My Profile</a>
+              <!-- <a href='ispsignup.php'>ISP-SIGNUP</a>
+              <a class='' href='isplogin.php'>ISP-LOGIN</a> -->
+              <a href='mypanel.php'>LogOut</a>
             </div>
         </div>
 
         <div class='mainbody'>
-            <div><h1 class='title'>DHAKA ISP SOLUTION</h1></div>
-            <div><h4>Search Your Desire ISP</h4></div>
+            <!-- <div><h1 class='title'>DHAKA ISP SOLUTION</h1></div> -->
+            <div><h4>Add Package Info</h4></div>
            
         </div>
 
         <div class='custom'>
-                <form action='customsearchresult.php' method='POST'>
+                <form action='packagetodatabase.php' method='POST'>
                   <div class='customform'>
                       
-                    <label for ='speed'>Select bandwith Speed</label>
-                    <input type='number' name='bandwith' id='speed' class=' placeholder='Enter a bandwith speed' min='1'  max='100' step='1' required>
+                    <label for ='speed'>Package Name</label>
+                    <input type='text' name='name' id='speed' class='' placeholder='Enter package name' required>
                     <br>
 
-                    <label for='pricing'>Select a price range</label>
-                    <input type='number' name='price' id='pricing' class=' placeholder='Enter a price range' min='100'  max='10000' step='1' required>
+                    <label for='pricing'>package Speed</label>
+                    <input type='number' name='speed' id='pricing' class='' placeholder='Enter package speed here' min='1'  max='10000' step='1' required>
                     <br>
 
-                    <label for='a'>Select Your Area</label>
-                    <input type='text' name='area' id='a' class=' placeholder='Enter your local area' required>
+                    <label for='a'>package Price</label>
+                    <input type='number' name='price' id='a' class='' placeholder='Enter package price' required>
                     <br>
 
-                    <label for='r'>Select By Rating</label>
-                    <input type='number' name='rating' id='r' class=' placeholder='Enter a minimum rating for isp' min='0'  max='5' step='1' required>
+                    <label for='r'>Offer Price</label>
+                    <input type='number' name='offer' id='r' class='' placeholder='Enter any offer Price' required>
 
-                    <button type='submit' class='ui violet button'>Search</button>
+                    <button type='submit' class='ui violet button'>ADD</button>
                   </div>
                 </form>
             </div>
